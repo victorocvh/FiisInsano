091149016821796a2d3badeb049a4ee076ac4845
Victor Carvalho <victor.ocv@hotmail.com>
2022-07-04 09:20:32 -0300

    up

--- src/app/login-template/login-signin/login-signin.component.ts
+++ src/app/login-template/login-signin/login-signin.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { Ativo } from 'src/app/models/ativo';
+import { FiiDB } from 'src/app/models/fii-db';
 import { Usuario } from 'src/app/models/usuario';
+import { AtivosService } from 'src/app/services/ativos.service';
+import { FiisService } from 'src/app/services/fiis.service';
 import { FirebaseAuthService } from 'src/app/services/firebase-auth.service';
 import { ModalWarnComponent } from 'src/app/shared/modal-warn/modal-warn.component';
 
@@ -15,10 +19,13 @@ export class LoginSigninComponent implements OnInit {
 
   public user: Usuario = new Usuario();
   public loading = false;
+  listaAtivos: Ativo[] = [];
 
   constructor(private fireAuthService: FirebaseAuthService,
     private router: Router,
-    private dialog: MatDialog) { }
+    private dialog: MatDialog,
+    private fiisService: FiisService,
+    private ativoService: AtivosService,) { }
 
   ngOnInit(): void {
   }
@@ -29,7 +36,49 @@ export class LoginSigninComponent implements OnInit {
       this.fireAuthService.signin(this.user).then(res => {
         console.log('RES:: ', res);
         this.loading = false;
-        this.router.navigateByUrl('h/home/index');
+
+
+        this.ativoService.getFiis().then((res) => {
+          console.log('RES:: ', res);
+          let somaAtivoTotal = res.map(atv => atv.atvQuantidade * atv.atvPrecoAtual)
+          .reduce((av1,av2) => av1 + av2, 0);
+          let ativosArray: Ativo[] = []
+          let listOfFiis = res.map(atv => atv.atvCod);
+          let listOfUniquesFiis = [...new Set(listOfFiis)];
+
+          listOfUniquesFiis.forEach(fiiUni => {
+            let ativo = new Ativo(null,null);
+            let findAtv = res.filter(x => x.atvCod == fiiUni);
+
+            if (findAtv) {
+              ativo.atvCod = fiiUni;
+              ativo.atvPrecoMedio = findAtv.map(atv => atv.atvPrecoAtual * atv.atvQuantidade).reduce((a,b) => a+b,0) / findAtv.map(atv => atv.atvQuantidade).reduce((a,b) => a+b,0);
+              ativo.atvAlocacao = (ativo.atvPrecoMedio * findAtv.map(atv => atv.atvQuantidade).reduce((a,b) => a+b,0) / somaAtivoTotal) * 100;
+              ativo.atvQuantidade = findAtv.map(atv => atv.atvQuantidade).reduce((a,b) => a+b,0);
+              ativo.atvValorTotal = ativo.atvQuantidade * ativo.atvPrecoMedio;
+            }
+            ativosArray.push(ativo);
+          });
+
+          this.fiisService.fiiDbList().then((res : FiiDB[]) => {
+            console.log('res db: ', res);
+            ativosArray.forEach(a => {
+              console.log('ATIVOS ARRAY: ', ativosArray)
+              let ativo = res.find(x => x.CodAtivo == a.atvCod);
+              console.log('ATIVO:: ', ativo);
+              if (ativo) {
+                a.atvPrecoMercado = Number(ativo.PrecoAtual.replace(',','.').replace('R$ ',''));
+              }
+            })
+            this.listaAtivos = ativosArray;
+          });
+
+          this.router.navigateByUrl('h/home/index');
+        })
+
+
+
+
       }).catch(error => {
         this.loading = false;
         this.dialog.open(ModalWarnComponent, {
